46efb9af6dd5b71bbcc97dc8d7674d3f6c792c6e
Michele Bertasi <michele.bertasi@gmail.com>
2018-08-10 18:22:47 +0200

    add travis file

--- bin/gmailfilter/main.go
+++ bin/gmailfilter/main.go
@@ -26,11 +26,15 @@ func readConfig(path string) (config.Config, error) {
 
 }
 
+func errorf(format string, a ...interface{}) {
+	/* #nosec */
+	_, _ = fmt.Fprintf(os.Stderr, format, a...)
+}
+
 func fatal(format string, a ...interface{}) {
-	fmt.Fprintf(os.Stderr, format, a...)
+	errorf(format, a...)
 	if !strings.HasSuffix(format, "\n") {
-		// Add newline
-		fmt.Fprintln(os.Stderr, "")
+		errorf("") // Add newline
 	}
 	os.Exit(1)
 }
