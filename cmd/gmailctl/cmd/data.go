5381efb554715b6fcc05f0765061d01b9b34c125
Michele Bertasi <michele.bertasi@gmail.com>
2019-02-07 23:13:03 +0100

    Embed standard library.
    In this way it's added by the init command by default.

---
+++ cmd/gmailctl/cmd/data.go
@@ -0,0 +1,13 @@
+//go:generate go run ../../../vendor/github.com/go-bindata/go-bindata/go-bindata -pkg cmd ../../../gmailctl.libsonnet
+
+package cmd
+
+// gmailctlLib returns the embedded gmailctl.libsonnet file
+func gmailctlLib() string {
+	return string(MustAsset("../../../gmailctl.libsonnet"))
+}
+
+// defaultConfig returns the embedded default configuration file
+func defaultConfig() string {
+	return string(MustAsset("../../../default-config.jsonnet"))
+}
