3e13a80c9e6e634944b2f82c4b899fb33d6609eb
Michele Bertasi <michele.bertasi@gmail.com>
2019-09-08 17:02:04 +0200

    Add apply tests.

--- pkg/apply/apply_test.go
+++ pkg/apply/apply_test.go
@@ -2,6 +2,7 @@ package apply
 
 import (
 	"flag"
+	"fmt"
 	"io/ioutil"
 	"math/rand"
 	"path/filepath"
@@ -89,6 +90,83 @@ func cfgPathToGmailConfig(t *testing.T, path string) (GmailConfig, error) {
 	}, err
 }
 
+func shuffleConfig(cfg GmailConfig) {
+	fs := cfg.Filters
+	rand.Shuffle(len(fs), func(i, j int) {
+		fs[i], fs[j] = fs[j], fs[i]
+	})
+	ls := cfg.Labels
+	rand.Shuffle(len(ls), func(i, j int) {
+		ls[i], ls[j] = ls[j], ls[i]
+	})
+}
+
+func addIDs(cfg GmailConfig) {
+	for i := range cfg.Filters {
+		cfg.Filters[i].ID = fmt.Sprintf("filter%d", i)
+	}
+	for i := range cfg.Labels {
+		cfg.Labels[i].ID = fmt.Sprintf("label%d", i)
+	}
+}
+
+func labelIDs(ls label.Labels) []string {
+	var ret []string
+	for _, l := range ls {
+		ret = append(ret, l.ID)
+	}
+	return ret
+}
+
+func filterIDs(fs filter.Filters) []string {
+	var ret []string
+	for _, f := range fs {
+		ret = append(ret, f.ID)
+	}
+	return ret
+}
+
+func toLabels(ms []label.ModifiedLabel) label.Labels {
+	var lbs label.Labels
+	for _, m := range ms {
+		label := m.New
+		label.ID = m.Old.ID
+		lbs = append(lbs, label)
+	}
+	return lbs
+}
+
+type fakeAPI struct {
+	addLabels     label.Labels
+	updateLabels  label.Labels
+	deleteLabels  []string
+	addFilters    filter.Filters
+	deleteFilters []string
+}
+
+func (f *fakeAPI) AddLabels(lbs label.Labels) error {
+	f.addLabels = lbs
+	return nil
+}
+
+func (f *fakeAPI) AddFilters(fs filter.Filters) error {
+	f.addFilters = fs
+	return nil
+}
+
+func (f *fakeAPI) UpdateLabels(lbs label.Labels) error {
+	f.updateLabels = lbs
+	return nil
+}
+func (f *fakeAPI) DeleteFilters(ids []string) error {
+	f.deleteFilters = ids
+	return nil
+}
+func (f *fakeAPI) DeleteLabels(ids []string) error {
+	f.deleteLabels = ids
+	return nil
+}
+
 func TestDiff(t *testing.T) {
 	upstream, err := cfgPathToGmailConfig(t, filepath.Join(testdataDir, "remote.jsonnet"))
 	assert.Nil(t, err)
@@ -101,12 +179,9 @@ func TestDiff(t *testing.T) {
 			diffFile := tps.diffs[i]
 			config := readConfig(t, local)
 
-			// Remote filters can come in _any_ order
+			// Remote filters and labels can come in _any_ order
 			// We can make the test more realistic by shuffling them here
-			remoteFilt := upstream.Filters
-			rand.Shuffle(len(remoteFilt), func(i, j int) {
-				remoteFilt[i], remoteFilt[j] = remoteFilt[j], remoteFilt[i]
-			})
+			shuffleConfig(upstream)
 
 			diff, err := Diff(config, upstream)
 			assert.Nil(t, err)
@@ -120,6 +195,46 @@ func TestDiff(t *testing.T) {
 				expectedDiff := read(t, diffFile)
 				assert.Equal(t, string(expectedDiff), diff.String())
 			}
+		})
+	}
+}
+
+func TestApply(t *testing.T) {
+	upstream, err := cfgPathToGmailConfig(t, filepath.Join(testdataDir, "remote.jsonnet"))
+	assert.Nil(t, err)
+	tps := allTestPaths(t)
+
+	for i := 0; i < len(tps.locals); i++ {
+		local := tps.locals[i]
+
+		t.Run(local, func(t *testing.T) {
+			config := readConfig(t, local)
+			api := fakeAPI{}
+
+			// Remote filters and labels can come in _any_ order
+			// We can make the test more realistic by shuffling them here
+			shuffleConfig(upstream)
+			addIDs(upstream)
+
+			diff, err := Diff(config, upstream)
+			assert.Nil(t, err)
+
+			// Apply without removing labels
+			err = Apply(diff, &api, false)
+			assert.Nil(t, err)
+			assert.Equal(t, diff.FiltersDiff.Added, api.addFilters)
+			assert.Equal(t, filterIDs(diff.FiltersDiff.Removed), api.deleteFilters)
+			assert.Equal(t, diff.LabelsDiff.Added, api.addLabels)
+			assert.Equal(t, toLabels(diff.LabelsDiff.Modified), api.updateLabels)
+			// Labels are never removed
+			assert.Empty(t, api.deleteLabels)
+
+			// Apply with removing labels
+			if len(diff.LabelsDiff.Removed) > 0 {
+				err = Apply(diff, &api, true)
+				assert.Equal(t, labelIDs(diff.LabelsDiff.Removed), api.deleteLabels)
+			}
+
 		})
 	}
 }
