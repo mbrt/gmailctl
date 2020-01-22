3206395c0402b67eb385b633a503d399a6aace9a
Michele Bertasi <michele.bertasi@gmail.com>
2020-01-22 22:54:30 +0100

    Rewrite evaluators.
    The evaluation is split in two steps:
    
    1. Creating a sane AST from a filter criteria;
    2. Evaluating the messages against the AST.
    
    Step 1 is the most complicated, since GMail has many quirks that
    they have to be mimicked with regular expresisons and expansions
    (not done yet).
    Step 2 is instead simple, since the evaluation itself will be
    straightforward.

--- pkg/cfgtest/cfgtest.go
+++ pkg/cfgtest/cfgtest.go
@@ -1,6 +1,9 @@
 package cfgtest
 
 import (
+	"fmt"
+	"regexp"
+
 	cfg "github.com/mbrt/gmailctl/pkg/config/v1alpha3"
 	"github.com/mbrt/gmailctl/pkg/parser"
 )
@@ -9,49 +12,115 @@ func Test(rules []parser.Rule, tests []cfg.Test) error {
 	return nil
 }
 
-func match(msg cfg.Message, c parser.CriteriaAST) (bool, error) {
-	eval := ruleEvaluator{msg: msg}
-	c.AcceptVisitor(&eval)
-	return eval.Match, eval.Err
+func NewEvaluator(criteria parser.CriteriaAST) (RuleEvaluator, error) {
+	v := evalBuilder{}
+	criteria.AcceptVisitor(&v)
+	return v.Res, v.Err
 }
 
-type ruleEvaluator struct {
-	msg   cfg.Message
-	Match bool
-	Err   error
+type RuleEvaluator interface {
+	Match(msg cfg.Message) bool
 }
 
-func (r *ruleEvaluator) VisitNode(n *parser.Node) {
-	if n.Operation == parser.OperationNot {
-		m, err := match(r.msg, n.Children[0])
-		r.Match, r.Err = !m, err
-		return
-	}
+type andNode struct {
+	children []RuleEvaluator
+}
 
-	r.Match = accumulateInit(n.Operation)
-	for _, child := range n.Children {
-		m, err := match(r.msg, child)
-		r.Match = accumulate(n.Operation, r.Match, m)
-		if r.Err == nil && err != nil {
-			// Never override the first error.
-			r.Err = err
+func (n andNode) Match(msg cfg.Message) bool {
+	for _, c := range n.children {
+		if !c.Match(msg) {
+			return false
 		}
 	}
+	return true
 }
 
-func (r *ruleEvaluator) VisitLeaf(n *parser.Leaf) {
-	// TODO
+type orNode struct {
+	children []RuleEvaluator
 }
 
-func accumulateInit(op parser.OperationType) bool {
-	// We need to start with a true only when we need to accumulate with
-	// AND. OR has to start with false and we don't care about NOT.
-	return op == parser.OperationAnd
-}
-
-func accumulate(op parser.OperationType, init, new bool) bool {
-	if op == parser.OperationAnd {
-		return init && new
+func (n orNode) Match(msg cfg.Message) bool {
+	for _, c := range n.children {
+		if c.Match(msg) {
+			return true
+		}
 	}
-	return init || new
+	return false
+}
+
+type notNode struct {
+	child RuleEvaluator
+}
+
+func (n notNode) Match(msg cfg.Message) bool {
+	return !n.child.Match(msg)
+}
+
+type funcNode struct {
+	op parser.FunctionType
+	re *regexp.Regexp
+}
+
+func (n funcNode) Match(msg cfg.Message) bool {
+	var fields []string
+
+	switch n.op {
+	case parser.FunctionFrom:
+		fields = []string{msg.From}
+	case parser.FunctionTo:
+		fields = msg.To
+	case parser.FunctionCc:
+		fields = msg.Cc
+	case parser.FunctionBcc:
+		fields = msg.Bcc
+	case parser.FunctionList:
+		fields = msg.Lists
+	case parser.FunctionSubject:
+		fields = []string{msg.Subject}
+	case parser.FunctionHas:
+		fields = []string{msg.Body}
+	}
+
+	for _, f := range fields {
+		if n.re.MatchString(f) {
+			return true
+		}
+	}
+
+	return false
+}
+
+type evalBuilder struct {
+	Res RuleEvaluator
+	Err error
+}
+
+func (r *evalBuilder) VisitNode(n *parser.Node) {
+	var children []RuleEvaluator
+	for _, c := range n.Children {
+		ce, err := NewEvaluator(c)
+		if err != nil {
+			r.Err = err
+			return
+		}
+		children = append(children, ce)
+	}
+
+	switch n.Operation {
+	case parser.OperationAnd:
+		r.Res = andNode{children}
+	case parser.OperationOr:
+		r.Res = orNode{children}
+	case parser.OperationNot:
+		if len(children) != 1 {
+			r.Err = fmt.Errorf("unexpected children size for 'not' node: %d", len(children))
+		}
+		r.Res = notNode{children[0]}
+	default:
+		r.Err = fmt.Errorf("unsupported operation %s", n.Operation)
+	}
+}
+
+func (r *evalBuilder) VisitLeaf(n *parser.Leaf) {
+	// TODO
 }
