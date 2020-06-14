936a1b78623e331033b5f16f84581aa39038eedc
Michele Bertasi <michele.bertasi@gmail.com>
2020-06-14 18:33:34 +0200

    Remove 'go generate' dependencies.
    The bindata dependency is used only at generate time; nothing the
    end user has to worry about. Fixed tests as well.

--- hack/check-codegen.sh
+++ hack/check-codegen.sh
@@ -4,4 +4,6 @@ set -e
 
 # check that we are not committing with things still to generate
 go generate ./...
+# `generate` adds additional dependencies that we want to remove.
+go mod tidy
 git diff --exit-code
