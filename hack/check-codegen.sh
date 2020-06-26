31a5666d90823324af725082e6e2d6a8e21552e8
Michele Bertasi <michele.bertasi@gmail.com>
2020-06-26 15:51:20 +0200

    Avoid checking go.sum
    The purpose is checking whether the `go gen` is up to date or not, not checking whether the dependencies are 100% clean.

--- hack/check-codegen.sh
+++ hack/check-codegen.sh
@@ -4,6 +4,4 @@ set -e
 
 # check that we are not committing with things still to generate
 go generate ./...
-# `generate` adds additional dependencies that we want to remove.
-go mod tidy
-git diff --exit-code
+git diff --exit-code -- pkg/ cmd/
