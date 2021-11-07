bd5dab1e2a48dfb4e302f77f0d0d0f9fc02056f9
Michele Bertasi <michele.bertasi@gmail.com>
2021-11-07 21:35:26 +0100

    Drop the codegen test.
    Not used anymore after moving away from go-bindata.

--- hack/check-codegen.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-# check that we are not committing with things still to generate
-go get github.com/go-bindata/go-bindata/go-bindata
-go generate ./...
-git diff --exit-code -- pkg/ cmd/
